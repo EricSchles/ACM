bf88313af7b2b53ade4c7be3f70415c2294e4cf4
EricSchles <ericschles@gmail.com>
2013-04-08 20:21:29 -0400

    function definitions created

---
+++ webdevHASH.py
@@ -0,0 +1,29 @@
+"""
+
+This simple website takes in your journal entries, stores it for you
+and allows you to edit it.
+
+This website is in flask.
+
+Stores data as a hash map
+"""
+
+from flask import Flask
+
+hash_map = {}
+
+
+
+
+
+#basic idea, fix this....
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
